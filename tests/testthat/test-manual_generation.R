b09911c6ae80f05255b9fb744a120eab510cf59d
MaxBox <boxm.psy@outlook.com>
2024-05-07 16:30:09 -0400

    verify seed functionality

--- tests/testthat/test-manual_generation.R
+++ tests/testthat/test-manual_generation.R
@@ -5,8 +5,19 @@ means <- c(5, 5, 5)
 sd <- c(1, 1, 1)
 effect_size <- c(1.5, 0.3, 0)
 
-test_that("assessment_gen creates dataframe", {
+test_that("manual_generation creates dataframe", {
   df <- manual_generation(Omega, n = 1000, means = means, sd = sd, effect_size)
   expect_true(is.data.frame(df))
 })
 
+test_that("seeding - same dataset", {
+  df1 <- manual_generation(Omega, n = 1000, means = means, sd = sd, effect_size, seed = 1)
+  df2 <- manual_generation(Omega, n = 1000, means = means, sd = sd, effect_size, seed = 1)
+  expect_true(identical(df1, df2))
+})
+
+test_that("seeding - differing datasets", {
+  df1 <- manual_generation(Omega, n = 1000, means = means, sd = sd, effect_size, seed = 1)
+  df2 <- manual_generation(Omega, n = 1000, means = means, sd = sd, effect_size, seed = 2)
+  expect_false(identical(df1, df2))
+})
